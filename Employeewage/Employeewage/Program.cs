0ffb0c6e02987d63f40fb6480f9f17317bf37d36
Vandanalalli06 <vandanalalli06@gmail.com>
2022-06-08 16:37:21 +0530

    [vandana] EMployeeprsntabsnt

--- Employeewage/Employeewage/Program.cs
+++ Employeewage/Employeewage/Program.cs
@@ -1,10 +1,12 @@
-﻿namespace program
+﻿namespace Employeewage
 {
     class Program
 {
     public static void Main(string[] args)
     {
             Console.WriteLine("Welcome to Employee wage Problem");
+            Employeewaage employeewage=new Employeewaage();
+            employeewage.CheckEmpPresentAbsent();
     }
 }
 }
