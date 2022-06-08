4b2b30a5322f16765d36b3ed182f1886a07509f0
Vandanalalli06 <vandanalalli06@gmail.com>
2022-06-08 17:32:35 +0530

    [Vandana]Empdailywage

--- Employeewage/Employeewage/Employeewaage.cs
+++ Employeewage/Employeewage/Employeewaage.cs
@@ -8,25 +8,37 @@ namespace Employeewage
 {
     internal class Employeewaage
     {
-        
-            public int Is_EmpPresent = 1;
-            public int Wage_Per_Hour = 20;
-            public int Fullday_Hour = 8;
-            public int Dailywage = 0;
-            public void CheckDailyEmpwage()
+
+        public int EmpPresent = 1;
+        public int Fulltime = 1;
+        public int Wage_Per_Hour = 20;
+        public int Fullday_Hour = 8;
+        public int Parttimeday_Hour = 4;
+        public int Dailywage = 0;
+        public void CheckParttimeEmpwage()
+        {
+            Random Empcheck = new Random();
+            int value = Empcheck.Next(0, 2);
+            if (value == EmpPresent)
             {
-                Random Empcheck = new Random();
-                int value = Empcheck.Next(0, 2);
-                if (value == Is_EmpPresent)
+                Random Timecheck = new Random();
+                int WorkingHours = Timecheck.Next(0, 2);
+                if (WorkingHours == Fulltime)
                 {
                     Dailywage = Fullday_Hour * Wage_Per_Hour;
-                    Console.WriteLine("Employee is present and Dailywage={0)", Dailywage);
+                    Console.WriteLine("Employee is present in full time and Dailywage={0)", Dailywage);
                 }
                 else
                 {
-                    Console.WriteLine("Employee is Absent");
+                    Dailywage -= Parttimeday_Hour * Wage_Per_Hour;
+                    Console.WriteLine("Employee is present in part time and Dailywage={0)", Dailywage);
                 }
             }
-        
+            else
+            {
+                Console.WriteLine("Employee is Absent");
+            }
+        }
+
     }
 }
